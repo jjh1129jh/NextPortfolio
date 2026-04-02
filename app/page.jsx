e200a3c4fa9f7437570795d0204d40c3357bf05c
jjh1129jh <jjh1129jh@naver.com>
2026-04-02 16:30:34 +0900

    vercel 업데이트 확인3

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,8 @@
 export default function HomePage() {
     return (
         <div>
-            <h1>포트폴리오 페이지입니다. 업데이트 확인</h1>
+            <h1>포트폴리오 페이지입니다.</h1>
+            <h3>업데이트 확인 항 아퍼</h3>
         </div>
     )
 }
