e2fdccc19fc574d78d7f99ccc225210ede47e76f
jjh1129jh <jjh1129jh@naver.com>
2026-04-02 16:59:28 +0900

    tailwind 연결 완료

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
     return (
         <div>
-            <h1>포트폴리오 페이지입니다.</h1>
+            <h1 className="text-blue-300">포트폴리오 페이지입니다.</h1>
         </div>
     )
 }
