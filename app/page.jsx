d570810253643a7c4a090192ea82eec66aefa267
jjh1129jh <jjh1129jh@naver.com>
2026-04-09 16:00:32 +0900

    오류 수정

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,19 @@
 import FullPageScroll from "../jsx/full_Page";
 
-// export const dataobj1 = "https://raw.githubusercontent.com/jjh1129jh/PotfolioJSON/refs/heads/main/PotfolioData_1.json";
-// export const dataobj2 = "https://raw.githubusercontent.com/jjh1129jh/PotfolioJSON/refs/heads/main/PotfolioData_2.json";
-export const dataobj1 = "/json/PotfolioData_1.json";
-export const dataobj2 = "/json/PotfolioData_2.json";
+export const dataobj1 = "https://raw.githubusercontent.com/jjh1129jh/PotfolioJSON/refs/heads/main/PotfolioData_1.json";
+export const dataobj2 = "https://raw.githubusercontent.com/jjh1129jh/PotfolioJSON/refs/heads/main/PotfolioData_2.json";
+// export const dataobj1 = "/json/PotfolioData_1.json";
+// export const dataobj2 = "/json/PotfolioData_2.json";
 
 export async function getDataA() {
-//   const responseA= await fetch(dataobj1, { cache: 'no-store' });
-    const responseA= await fetch(`http://localhost:3000${dataobj1}`, { cache: 'no-store' });
+  const responseA= await fetch(dataobj1, { cache: 'no-store' });
+    // const responseA= await fetch(`http://localhost:3000${dataobj1}`, { cache: 'no-store' });
     return responseA.json();
 }
 
 export async function getDataB() {
-    // const responseB = await fetch(dataobj2, { cache: 'no-store' });
-    const responseB = await fetch(`http://localhost:3000${dataobj2}`, { cache: 'no-store' });
+    const responseB = await fetch(dataobj2, { cache: 'no-store' });
+    // const responseB = await fetch(`http://localhost:3000${dataobj2}`, { cache: 'no-store' });
     return responseB.json();
 }
 
